8f704a2bb372d19704280b8ef3bc026687e4e85a
Harsh-Ranjan05 <harshranjan1644@gmail.com>
2025-06-27 13:16:43 +0530

    Database Query Added

--- db.php
+++ db.php
@@ -4,4 +4,5 @@ $username = "postgres";
 $password = "5625";
 $db = "PROFILE";
 $conn = pg_connect("host=$server dbname=$db user=$username password=$password");
+/* Database Query Added Sucessfully*/
 ?>
